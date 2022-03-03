031731f3f969ab05a8170dcbcaaddfc4a27ebe18
jhorbino93 <j.e.horbino@gmail.com>
2022-03-03 20:40:00 +1000

    upd maintenance tables

--- dbMaster/dbETL/Curated/020_LoadCurated_Dimensions.R
+++ dbMaster/dbETL/Curated/020_LoadCurated_Dimensions.R
@@ -34,16 +34,17 @@ default_end_date = as_date("2099-12-31")
 current_date = as_date(Sys.Date())
 default_prior_date = current_date - days(1)
 
+## Github directory
+base_github <- "https://raw.githubusercontent.com/jhorbino93/ShinyHermes/main/dbMaster"
+ref_dir <- "/dbReference"
 
-## Load Functions
+## Load Functions ----
 url_r_misc_fn <- "https://raw.githubusercontent.com/jhorbino93/ShinyHermes/main/r_functions/misc_functions.R"
 source_url(url_r_misc_fn)
 
 url_r_hmy_fn <- "https://raw.githubusercontent.com/jhorbino93/ShinyHermes/main/r_functions/hmy_functions.R"
 source_url(url_r_hmy_fn)
 
-## Local data directory
-raw_dir <- "C:/Users/jehor/Documents/GitHub/Hermes/dbMaster/dbData/001Raw"
 
 ## Github directory
 base_github <- "https://raw.githubusercontent.com/jhorbino93/ShinyHermes/main/dbMaster"
@@ -90,107 +91,16 @@ maintenance_account_balance <- read.csv(
 
 
 
-  
-## Load dim_assets ----  
-  tmp_asset1 <- 
-    as_tibble(maintenance_dim_ticker) %>%
-    ## Add other attributes 
-    mutate(
-      masterchef_address = NA_character_
-      ,onchain_address = ticker_name
-      ,pid = NA_integer_
-      ,asset_type_l1 = case_when(
-        ticker_src_cat == "cex"~"CEX"
-        ,ticker_src_cat == "dex"~"On-chain"
-      )
-      ,asset_type_l2 = case_when(
-        ticker_src_cat == "cex"~"CEX ticker"
-        ,ticker_src_cat == "dex"~"LP"
-        ,T ~ "Other"
-      )
-      ,asset_type_l3 = asset_type_l2
-      ,onchain_network = ticker_src_network
-      ,asset_alias = short_name
-      ,asset_name = ticker_alias
-    ) %>%
-    ## Select order
-    select(
-      asset_name
-      ,short_name
-      ,asset_alias
-      ,asset_type_l1
-      ,asset_type_l2
-      
-      ## Ticker related columns
-      ,ticker_name
-      ,ticker_alias
-      ,ticker_src_cat
-      ,ticker_src_network
-      ,asset1
-      ,asset2
-      ,data_src
-      ,exchange_name
-      
-      ## On chain stuff
-      ,onchain_network
-      ,onchain_address
-    ) %>%
-    
-    ## Rename output 
-    rename(
-      asset_to = asset1
-      ,asset_from = asset2
-      ,asset_short_name = short_name
-    )
-  
-  tmp_asset2 <- as_tibble(maintenance_pid) %>%
-    filter(!address %in% tmp_asset1$onchain_address) %>%
-    mutate(
-      ticker_name = address
-      ,asset_short_name  = friendly_alias
-      ,asset_alias = friendly_alias
-      ,asset_type_l1 = "On-chain"
-      ,asset_type_l2 = product_type
-      ,asset_type_l3 = asset_type_l2
-      ,ticker_alias = product_name
-      ,ticker_src_cat = case_when(
-        product_type == "LP" ~ "dex"
-        ,product_type %in% c("HRC20") ~ "address"
-        ,T ~ "Other"
-      )
-      ,ticker_src_network = network
-      ,asset_to = NA_character_
-      ,asset_from = NA_character_
-      ,data_src = "On-chain"
-      ,onchain_network = network
-      ,onchain_address = address
-      ,exchange_name = dex_platform
-    ) %>%
-    rename(
-      asset_name = product_name
-    ) %>%
-    select_at(colnames(tmp_asset1))
+dim_asset <- arrow::read_parquet(paste0(c(dir_cur,"dim_asset.parquet"),collapse="/"))
 
-  
-  dim_asset <- bind_rows(tmp_asset1,tmp_asset2) %>%
-    select_at(colnames(tmp_asset1))
-  rm(list=c("tmp_asset1","tmp_asset2"))
-  
-  vct_pk_dim_asset <- c("asset_name","ticker_name","ticker_src_network","data_src")
-  
-  ## Get existing state 
-  old_dim_asset <- arrow::read_parquet(paste0(c(dir_cur,"dim_asset.parquet"),collapse="/"))
-  
-  ## Match & merge
-  dim_asset <- fn_db_merge_dim(dim_asset,old_dim_asset,vct_pk_dim_asset,"dim_asset_id")
-  
-  ## Write to dir
-  arrow::write_parquet(dim_asset,paste0(c(dir_cur,"dim_asset.parquet"),collapse="/"))
+as_tibble(maintenance_pid) %>%
+  filter(product_type == "LP") %>%
+  select(masterchef_address,product_name,product_type,pid,address,friendly_alias,network)
+
+as_tibble(maintenance_masterchef)
 
 
-## Load dim products ----
-unique(c(dim_ticker$asset1,dim_ticker$asset2))
-  
+
 ## Binance ----
 vct_binance_assets <- list.files(paste0(dir_raw,"/binance"))
 
